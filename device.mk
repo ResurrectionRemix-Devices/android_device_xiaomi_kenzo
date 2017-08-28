12a0e9e2685730b825e8127f72917cb121990a7b
TheScarastic <warabhishek@gmail.com>
2017-08-29 00:00:55 +0530

    kenzo: Copy hals. conf to vendor
     * HIDL expects it there

--- device.mk
+++ device.mk
@@ -42,8 +42,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/vendor/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
 
 # Wifi
 PRODUCT_COPY_FILES += \
